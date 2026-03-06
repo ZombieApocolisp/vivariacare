adee1895ead62c92f9eec65614c7c280fc6b79b2
Brandon Ramos <597bramos@gmail.com>
2026-03-06 15:58:31 -0500

    Removed styling and old commented code from the index.tsx file.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { startMockPolling } from "@/src/state/terrariumThunks";
 import React, { useEffect } from "react";
-import { StyleSheet } from "react-native";
 import DashboardScreen from "../../src/screens/DashboardScreen";
 import { useAppDispatch, useAppSelector } from "../../src/state/hooks";
 
@@ -17,57 +16,4 @@ export default function DashboardRoute() {
   const secondsAgo = Math.floor((Date.now() - status.updatedAt) / 1000);
 
   return <DashboardScreen />;
-
-  // return (
-  //   <View style={styles.container}>
-  //     <Text style={styles.title}>Terrarium Dashboard</Text>
-  //     <Text style={styles.update}>Last updated: {secondsAgo}s ago</Text>
-
-  //     <SensorTile
-  //       label="Temperature"
-  //       value={`${status.temperatureF.toFixed(1)} °F`}
-  //     />
-  //     <SensorTile label="Humidity" value={`${status.humidityPct}%`} />
-  //     <SensorTile
-  //       label="Substrate Moisture"
-  //       value={`${status.substrateMoisturePct}%`}
-  //     />
-  //     <SensorTile label="Reservoir" value={`${status.reservoirPct}%`} />
-  //     <SensorTile label="Pump" value={status.pumpOn ? "ON" : "OFF"} />
-  //     <SensorTile label="Lights" value={status.lightsOn ? "ON" : "OFF"} />
-  //   </View>
-  // );
 }
-
-/**
- * Utility helpers (pure functions)
- * These are NOT React-specific. They just help generate nice fake data.
- */
-
-// function randomBetween(min: number, max: number) {
-//   return Math.random() * (max - min) + min;
-// }
-
-// function clamp(value: number, min: number, max: number) {
-//   return Math.min(max, Math.max(min, value));
-// }
-
-// function round1(value: number) {
-//   return Math.round(value * 10) / 10;
-// }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    gap: 12,
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: "700",
-  },
-  update: {
-    fontSize: 14,
-    opacity: 0.7,
-  },
-});
